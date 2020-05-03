c2e989aebf36b5deac5a10bf8914294da62f5bea
Bernhard Krämer <bernhard.kraemer1991@gmail.com>
2020-05-03 21:20:29 +0200

    Added model for table ADRESSEN including test.

---
+++ src/Models/Adresse.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Bios2000\Models;
+
+use Eloquent;
+
+/**
+ * Class Adresse
+ * @package Bios2000\Models
+ * @mixin Eloquent
+ */
+class Adresse extends Bios2000Master
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'ADRESSEN';
+
+    /**
+     * Primary Key
+     *
+     * @var string
+     */
+    protected $primaryKey = "KUNU";
+
+
+//    /**
+//     * Get contact persons of address
+//     *
+//     * @return ContactPerson
+//     */
+//    public function contactpersons()
+//    {
+//        return $this->hasMany('Bios2000\Models\Database\ContactPerson', 'KUNU', 'KUNU');
+//    }
+}
--- tests/Unit/AdressenTest.php
+++ tests/Unit/AdressenTest.php
@@ -2,16 +2,38 @@
 
 namespace Bios2000\Tests\Unit;
 
-use Tests\TestCase;
+use Bios2000\Models\Adresse;
+use Bios2000\Tests\TestCase;
 
 class AdressenTest extends TestCase
 {
 
     /** @test */
-    function a_address_model_includes_bios_address_data()
+    function a_address_model_has_needed_columns()
     {
-        $address = new Adresse;
+        $expectedDatabaseColumns = [
+            'KUNU', 'KB', 'ANSCHRIFT_1', 'ANSCHRIFT_2', 'ANSCHRIFT_3', 'ZUSATZ', 'STRASSE', 'ADRESSE', 'LAND',
+            'POSTFACH', 'POSTFACH_ADRESSE', 'BEMERKUNG', 'TELEFON', 'TELEFAX', 'BTX', 'ISDN_NUMMER', 'FUNKTELEFON',
+            'EMAIL_ADRESSE', 'RE_EMPF', 'FIBU_SA_KONTO', 'UST_ID_NR', 'EXT_KUNU', 'BANK', 'KONTO_NUMMER', 'BLZ',
+            'KREDIT_LIMIT', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'SPRACHE', 'VERTRETER_1',
+            'VERTRETER_2', 'ZB', 'VB', 'PS', 'BS_1', 'BS_2', 'MAHNSPERRE', 'LASTSCHRIFT_KZ', 'SR_KZ', 'WS',
+            'ANLAGE_DATUM', 'AENDERUNG_DATUM', 'ANLAGE_USER_NR', 'AENDERUNG_USER_NR', 'KOND_KUNU', 'RESERVE_INTEGER',
+            'RESERVE_CURRENCY', 'RESERVE_STRING', 'STATUS', 'BIC', 'IBAN', 'BEMERKUNG_2', 'ILN_NUMMER', 'STD_BANK',
+            'WEEE', 'ADR_PLZ', 'ADR_ORT', 'IS_SAMMLER'
+        ];
 
-        dd($address);
+        $this->assertModelHasColumns($expectedDatabaseColumns, Adresse::class);
     }
+
+    /*
+     * TODO:
+     * - Relationship > ANSPRECHPARTNER
+     * - Attribut VERTRETER_1_NAME
+     * - Attribut ZB_NAME
+     * - Attribut VB_NAME
+     * - Attribut ANLAGE_USER_NAME
+     * - Attribut AENDERUNG_USER_NAME
+     * - Attribut STATUS
+     * - Attribut isLiefersperre (englisch!) (KREDIT_LIMIT == 1.11)
+     */
 }
